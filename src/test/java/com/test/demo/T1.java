f3c5f54312ff40033742d102ac81cb78a92d2636
Abhishek Gupta <abhishekgupta183@gmail.com>
2019-07-09 23:14:09 +0530

    fifth

--- src/test/java/com/test/demo/T1.java
+++ src/test/java/com/test/demo/T1.java
@@ -4,7 +4,10 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class T1 {
@@ -18,9 +21,12 @@ public class T1 {
 		driver.manage().window().maximize();
 		driver.get("https://www.nopcommerce.com/");
 		driver.findElement(By.xpath("//ul[@class='header-links-account']//a[@class='ico-register'][contains(text(),'Register')]")).click();
-		Thread.sleep(10);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement element = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='ctl00_ctl00_cph1_cph1_ctrlCustomerRegister_CreateUserForm_CreateUserStepContainer_txtFirstName']")));
 		driver.findElement(By.xpath("//input[@id='ctl00_ctl00_cph1_cph1_ctrlCustomerRegister_CreateUserForm_CreateUserStepContainer_txtFirstName']")).sendKeys("Abhishek");
 		
 	}
-
+	
+	
 }
